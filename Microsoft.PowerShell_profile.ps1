5b7a6963273a1ed1c2ccfdda0eea406c82df8aa1
F4NT0 <fantolaboratorio@hotmail.com>
2023-08-31 21:08:38 -0300

    Update README.md

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,11 @@
- oh-my-posh init pwsh --config 'C:\Users\Gabriel_Stundner\AppData\Local\Programs\oh-my-posh\themes\craver.omp.json' | Invoke-Expression
+# ---------------------------
+#  POWERSHELL CONFIGURATION
+# ---------------------------
+
+# ALIAS
+New-Alias c -Value clear
+
+# OH-MY-POSH CONFIGURATION
+# choco install oh-my-posh
+# Get-Themes show all themes available
+oh-my-posh init pwsh --config 'C:\Users\Gabriel_Stundner\AppData\Local\Programs\oh-my-posh\themes\craver.omp.json' | Invoke-Expression
